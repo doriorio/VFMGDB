948d3de9d1f767840dd4d0aa4e7eba821cb55df1
doriorio <itsdori@gmail.com>
2021-06-15 08:34:08 -0700

    working through db

---
+++ app/documents.py
@@ -0,0 +1,15 @@
+from app import db
+
+class ShoppingList(db.document):
+    name = db.StringField()
+    quantity = db.FloatField()
+    unit = db.StringField()
+    missing = db.BoolField()
+
+    def __dict__(self):
+        return {
+            'name': self.name,
+            'quantity': self.quantity,
+            'unit': self.unit,
+            'missing': self.missing
+        }
--- config.py
+++ config.py
@@ -2,3 +2,10 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET>KEY') or "my_secret_key"
+    MONGOALCHEMY_DATABASE = "shoppingList"
+    MONGODB_DB = 'shoppingList'
+    MONGODB_HOST = '0.0.0.0'
+    MONGODB_PORT = 27017
+    ENV = 'development'
+    SERVER_NAME = '0.0.0.0:5000'
+    DEBUG = True
